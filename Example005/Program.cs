70e1e11f935b5efd404ed045e451dbaff5d019a3
Daniil S <daniil.2210s@gmail.com>
2023-09-18 11:16:25 +0300

    Ex005 User

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Type usermane: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "user")
+{
+    Console.WriteLine("Yuppi, that's you!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
